a4586be7054c52ed6a9788330c4ca579c7048d31
Darcy Murphy <mrDarcyMurphy@gmail.com>
2015-07-13 19:14:16 -0500

    Add config/prod.secret.exs

---
+++ config/prod.secret.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :my_app, MyApp.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL")
+  # username: "postgres",
+  # password: "postgres",
+  # database: "my_app_prod",
+  size: 20 # The amount of database connections in the pool
